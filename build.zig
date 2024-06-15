ded196a292b79a3733b820fc8aca300dca2ff8a2
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2024-06-15 19:42:03 -0400

    chore: Port the original SGE project functionality to Zig (#1)
    * feat: Port GFX functionality
    
    * feat: Move the example application to the example file
    
    * feat: Restructure core functionality; Add example app

--- build.zig
+++ build.zig
@@ -4,50 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-
-    const sge = b.addStaticLibrary(.{
-        .name = "sge",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(sge);
-
-    const sge_module = b.addModule("sge", .{
-        .root_source_file = b.path("src/root.zig"),
-    });
     const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize });
-
-    sge_module.addIncludePath(raylib.path("src"));
-    sge_module.linkLibrary(raylib.artifact("raylib"));
-
-    const example = b.addExecutable(.{
-        .name = "sge_example",
-        .root_source_file = b.path("src/example.zig"),
-        .target = target,
-        .optimize = optimize,
+    const zge_module = b.addModule("zge", .{
+        .root_source_file = b.path("src/core.zig"),
     });
-    example.root_module.addImport("sge", sge_module);
-    b.installArtifact(example);
-
-    // "zig build run" -----------------------------------------------------------------------------
-
-    const run_cmd = b.addRunArtifact(example);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    zge_module.addIncludePath(raylib.path("src"));
+    zge_module.linkLibrary(raylib.artifact("raylib"));
 
     // "zig test" ----------------------------------------------------------------------------------
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/core.zig"),
         .target = target,
         .optimize = optimize,
     });
